2dd42675f2931dbaa4612525d1a0e17591376456
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-12-22 16:16:18 -0700

    facebook OAuth works with user data saved ininto API

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
+var FacebookStrategy = require('passport-facebook');
+var passport = require('passport');
+
+var db = require('monk')('localhost/bombroller-users');
+var users = db.get('users');
+
+require('dotenv').load();
+passport.authenticate();
+
 
 var routes = require('./routes/index');
 
@@ -20,9 +30,89 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: process.env.COOKIE_SESSION_NAME,
+  keys: [process.env.KEY1, process.env.KEY2]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    enableProof: false,
+    profileFields: ['id', 'displayName', 'link', 'photos', 'email']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // console.log(profile);
+    var fullName = profile.displayName.split(" "),
+        userFirstName = fullName[0],
+        userLastName = fullName[1],
+        userPhoto = profile.photos[0].value;
+
+    users.findOne({ fbid: profile.id}).then(function(user){
+        console.log("USER", user);
+        if(user == null){
+        users.insert({
+            fbid: profile.id,
+            firstname: userFirstName,
+            lastname: userLastName,
+            profilepic: userPhoto
+          }, function (err, doc) {
+
+            if (err) throw err;
+          });
+        }else{
+          users.findOne({fbid: profile.id}).on('success', function (doc) {
+            done(null, { facebookId: profile.id, firstName: userFirstName, lastName: userLastName, token: accessToken });
+          });
+        }
+    })
+  }
+));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/logout', function(req, res){
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  res.locals.user = req.user
+  next()
+})
+
 app.use('/', routes);
+
+app.get('/api/v1/leaderboard', function(req, res){
+
+  users.find({}, function (err, docs){
+    console.log(docs);
+    res.json(docs);
+  })
+
+});
 
 app.get('*', function(req, res){
     res.redirect('/index.html');
