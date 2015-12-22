d5292cad328b89d6d6d9bd5dcec31e1d9899ec7d
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-12-22 14:03:02 -0700

    basic css with sticky header links

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,8 +1,11 @@
 app.controller('IndexController', ['$scope', function($scope) {
     $scope.greeting = 'Welcome to Landmine Man!';
   }]);
-app.controller('HomeController', ['$scope', function($scope) {
+app.controller('HomeController', ['$scope', '$window', function($scope, $window) {
     $scope.greeting = 'Welcome to Landmine Man!';
+    var test = $window.navigator.userAgent;
+    console.log(test);
+
   }]);
 app.controller('AboutController', ['$scope', function($scope) {
     $scope.greeting = 'Welcome to Landmine Man!';
