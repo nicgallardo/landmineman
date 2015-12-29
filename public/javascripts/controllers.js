c109abf42f8822fd3ac43455e5386822738624f5
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-12-29 14:08:39 -0700

    able to have interaction through controller, coordinance are a bit off

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -46,22 +46,108 @@ app.controller('LeadersController', ['$scope', function($scope) {
 }]);
 
 app.controller('GamesController', ['$scope', '$http',  function($scope, $http) {
+  //access the dom div
+  var ball   = document.querySelector('.ball');
+  var garden = document.querySelector('.garden');
+  var output = document.querySelector('.output');
+  var score = document.querySelector('.score');
+  var blackHoleLocation = document.querySelector('.blackhole-location')
+  var domHoleLocation = document.querySelector('.domHole-location')
+  var maxX = garden.clientWidth  - ball.clientWidth;
+  var maxY = garden.clientHeight - ball.clientHeight;
+  var blackHole = {
+    x: 16,
+    y: 16
+  }
+
+  var point = 1;
+  //need to loop through bombs object constantly to see if they are hit
+  //needs to loop through bombs to make sure new bomb isnt placed where the hole is when player achieves point
+  //create bomb needs to loop through the bombs to make sure bombs are not placed on top of another
+  function handleOrientation(event) {
+    var x = Math.floor(event.beta);
+    var y = Math.floor(event.gamma);
+
+    for (var key in bombs) {
+      console.log((bombs[key].x), x);
+      if((bombs[key].x-10) == x && (bombs[key].y - 10) == y){
+        // console.log("Dead");
+      }
+    }
+
+    if(blackHole.x == x && blackHole.y == y) {
+      score.innerHTML = point;
+      point++;
+      changeHole();
+    }
+
+    output.innerHTML = "beta : " + x + "\n";
+    output.innerHTML += "gamma: " + y + "\n";
+
+    if (x >  90) { x =  90};
+    if (x < -90) { x = -90};
+
+    x += 90;
+    y += 90;
+
+    ball.style.top  = (maxX*x/180 - 10) + "px";
+    ball.style.left = (maxY*y/180 - 10) + "px";
+  }
+
+  function correctNumb(numb){
+    if(numb == 0 ) return 90;
+    if(numb == 90) return 0;
+    return numb > 90 ? (numb-80) : -(-numb + 80);
+  }
+
+  function changeHole() {
+    var xCoord = Math.floor(Math.random()*(10, 170));
+    var yCoord = Math.floor(Math.random()*(10, 170));
+    blackHole.x = correctNumb(xCoord);
+    blackHole.y = correctNumb(yCoord);
+
+    var hole = document.querySelector('.hole');
+    hole.style.top = xCoord + 'px';
+    hole.style.left = yCoord + 'px';
+
+    blackHoleLocation.innerHTML = "blackHole.x : " + blackHole.x + "  blackHole.y : " + blackHole.y;
+    domHoleLocation.innerHTML = "dom.xCoord : " + xCoord + "  dom.yCoord : " + yCoord;
+
+    createBomb();
+  }
+  var bombs = [];
+  function createBomb() {
+    //10, 170
+    var xCoord = Math.floor(Math.random()*(30, 150));
+    var yCoord = Math.floor(Math.random()*(30, 150));
+
+    bombX = correctNumb(xCoord);
+    bombY = correctNumb(yCoord);
+
+    var tempBomb = [];
+    tempBomb.push(bombX,bombY);
+    bombs.push(tempBomb);
+
+    var landmine = "<div class='bomb' style='top:" + xCoord+ "px; left:"+ yCoord +"px;'></div>";
+    var bomb = document.createElement('div');
+    bomb.innerHTML = landmine;
+    garden.appendChild(bomb);
+  }
+
+  window.addEventListener('deviceorientation', handleOrientation);
+
+  // below increments the score only
   $scope.count = 0;
   $scope.increment = function(){
-
     $scope.count++;
-
     var dataObj = {
       score : $scope.count,
     }
-
     $http.post('/api/v1/add-point', dataObj).
     success(function(data) {
         console.log("posted successfully: ", data);
     }).error(function(data) {
         console.error("error in posting: ", data);
     })
-
   }
-
 }]);
